2c9a9c34e8bf43f44c2ab8a0681191563ed26c22
Ricardo Rubio <42851737+RicardoRubio11@users.noreply.github.com>
2020-09-25 09:33:49 -0600

    Update for Spanish translate

--- src/translations/loader.js
+++ src/translations/loader.js
@@ -1,9 +1,11 @@
 import langEN from './en';
 import langDE from './de';
 import langFR from './fr';
+import langFR from './es';
 
 export default {
   en: langEN,
   de: langDE,
   fr: langFR,
+  es: langES,
 };
